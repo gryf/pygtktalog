c600719c83329ac24a3631eec34c2f8195cedf01
gryf <devnull@localhost>
2007-05-05 20:18:52 +0000

    * View for configuration.

---
+++ mvc/src/views/v_config.py
@@ -0,0 +1,15 @@
+# This Python file uses the following encoding: utf-8
+
+import utils._importer
+import utils.globals
+from gtkmvc import View
+import os.path
+
+class ConfigView(View):
+    """Preferences window from glade file """
+    GLADE = os.path.join(utils.globals.GLADE_DIR, "config.glade")
+    def __init__(self, ctrl):
+        View.__init__(self, ctrl, self.GLADE)
+        return
+
+    pass # end of class
